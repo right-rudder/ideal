72d427c4470fa997af4b4883f2072dc149ceac66
Tim Jedrek <tim@timjedrek.com>
2023-04-19 13:25:45 -0500

    create airplane pilot training page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,4 +16,7 @@ class PagesController < ApplicationController
 
   def rotary_wing
   end
+
+  def airplane_training
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   get 'aircraft-rental', to: 'pages#aircraft_rental', as: :aircraft_rental
   get 'fixed-wing-airplane-fleet', to: 'pages#fixed_wing', as: :fixed_wing
   get 'rotary-wing-helicopter-fleet', to: 'pages#rotary_wing', as: :rotary_wing
+  get 'airplane-pilot-training', to: 'pages#airplane_training', as: :airplane_training
 
 end
