14e58df817fb12f3225a535b5a9ea18f07e3736b
Tim Jedrek <tim@timjedrek.com>
2023-04-21 22:53:37 -0500

    add routes for new pages

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,9 @@ Rails.application.routes.draw do
   get 'airplane-pilot-training', to: 'pages#airplane_training', as: :airplane_training
   get 'helicopter-pilot-training', to: 'pages#helicopter_training', as: :helicopter_training
   get 'private-pilot-training', to: 'pages#private_pilot', as: :private_pilot
+  get 'about-ideal-aviation', to: 'pages#about_us', as: :about_us
+  get 'advanced_training', to: 'pages#advanced_training', as: :advanced_training
+  get 'our-story', to: 'pages#our_story', as: :our_story
+  get 'our-team', to: 'pages#out_team', as: :our_team
 
 end
