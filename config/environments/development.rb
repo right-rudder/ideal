2401bb0c718f0b3cd29bc8149d17fb53b19c6b5b
Tim Jedrek <tim@timjedrek.com>
2023-04-23 23:32:12 -0500

    configs for action mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,4 +70,18 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "email-smtp.us-east-1.amazonaws.com",
+    port: 587,
+    domain: "idealaviationstlouis.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "AKIA3EJFDQDTYZTKN7PM",
+    password: ENV["AMAZON_SES_PASS"]
+  }
 end
