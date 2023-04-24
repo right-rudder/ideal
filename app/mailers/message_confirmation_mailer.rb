5af7d75dea04f8627e2c20820cb36e8bcf2f0557
Tim Jedrek <tim@timjedrek.com>
2023-04-23 23:44:39 -0500

    clean up mailer stuff

--- app/mailers/message_confirmation_mailer.rb
+++ app/mailers/message_confirmation_mailer.rb
@@ -2,9 +2,10 @@ class MessageConfirmationMailer < ApplicationMailer
 
   default from: 'no-reply@idealaviationstlouis.com'
 
-  def message_confirmation_email
-    @message = params[:message]
+  def message_confirmation_email(message)
+    @message = message
 
     mail(to: @message.email, subject: "✈️ Thank you for contacting Ideal Aviation")
+  end
 
 end
