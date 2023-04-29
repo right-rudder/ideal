de7ff5b561090b27b8c4e0f4c3b53b7e036c44d2
Tim Jedrek <tim@timjedrek.com>
2023-04-28 21:33:17 -0500

    migrate the phone into quiz_results

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_23_064037) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_29_023103) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -73,6 +73,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_23_064037) do
     t.text "comments"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone"
   end
 
 end
