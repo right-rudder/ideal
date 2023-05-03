54e8c0439ec36dfc58c87634486a1c054bf637e5
Tim Jedrek <tim@timjedrek.com>
2023-05-03 01:29:16 -0500

    add validation error message

--- app/models/quiz_result.rb
+++ app/models/quiz_result.rb
@@ -1,3 +1,5 @@
 class QuizResult < ApplicationRecord
   validates :previous_training, presence: true, unless: -> { previous_training.blank? }
+  validates :phone, presence: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
 end
