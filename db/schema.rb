df9c1be62b8b9a5016db84b2db481870dc2d1fc2
Tim Jedrek <tim@timjedrek.com>
2023-05-03 00:54:39 -0500

    remove approach (go around lol)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_03_053433) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_03_055336) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -73,7 +73,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_03_053433) do
     t.string "phone"
     t.string "learn_style"
     t.string "structure"
-    t.string "approach"
     t.text "guidance"
   end
 
