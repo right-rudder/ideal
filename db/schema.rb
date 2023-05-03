a36a15e2e4b4761dd39f5a5efdfed2657aa3281d
Tim Jedrek <tim@timjedrek.com>
2023-05-03 00:40:48 -0500

    fix db by removing bools and then convert guidance in2 text

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_02_234556) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_03_053433) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -58,13 +58,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_02_234556) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
-    t.boolean "in_us"
     t.string "program"
     t.text "why_pilot"
     t.string "previous_training"
     t.text "wings_lift"
     t.text "goals"
-    t.boolean "schedule_visit"
     t.date "preferred_date"
     t.string "preferred_availability"
     t.date "alternate_date"
@@ -76,7 +74,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_02_234556) do
     t.string "learn_style"
     t.string "structure"
     t.string "approach"
-    t.string "guidance"
+    t.text "guidance"
   end
 
 end
