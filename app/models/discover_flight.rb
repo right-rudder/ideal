79f79330c911955d3966d1a024cb1a5f5fefd768
Tim Jedrek <tim@timjedrek.com>
2023-05-04 21:13:03 -0500

    add validation

--- app/models/discover_flight.rb
+++ app/models/discover_flight.rb
@@ -1,4 +1,6 @@
 class DiscoverFlight < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   validates :phone, presence: true
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
 end
--- app/models/quiz_result.rb
+++ app/models/quiz_result.rb
@@ -1,5 +1,7 @@
 class QuizResult < ApplicationRecord
-  validates :previous_training, presence: true, unless: -> { previous_training.blank? }
+  #validates :previous_training, presence: true, unless: -> { previous_training.blank? }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   validates :phone, presence: true
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
 end
