c9a3b1c1e6a63fe536b8dc4f911f7cc696f9e475
Tim Jedrek <tim@timjedrek.com>
2023-05-05 00:22:44 -0500

    try out phone validation

--- app/models/quiz_result.rb
+++ app/models/quiz_result.rb
@@ -1,7 +1,13 @@
 class QuizResult < ApplicationRecord
   #validates :previous_training, presence: true, unless: -> { previous_training.blank? }
+  before_validation :strip_phone_number
+  
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :phone, presence: true
+  validates :phone, presence: true, length: { minimum: 7 }
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
+
+  def strip_phone_number
+    self.phone = phone.to_s.gsub(/[-() ]/, "")
+  end
 end
