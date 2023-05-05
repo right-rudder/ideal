a723d762be0fec5a20faa0b5ddf24247dbbc5b7c
Tim Jedrek <tim@timjedrek.com>
2023-05-04 23:39:03 -0500

    add name phone email to migration

--- db/migrate/20230505043634_create_book_downloads.rb
+++ db/migrate/20230505043634_create_book_downloads.rb
@@ -4,5 +4,9 @@ class CreateBookDownloads < ActiveRecord::Migration[7.0]
 
       t.timestamps
     end
+    
+    add_column :book_downloads, :name, :string
+    add_column :book_downloads, :phone, :string
+    add_column :book_downloads, :email, :string
   end
 end
