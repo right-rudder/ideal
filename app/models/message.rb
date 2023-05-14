ca14e63f4bf6952c43b5c5ff9a37b80d6540f53d
Tim Jedrek <tim@timjedrek.com>
2023-05-14 09:50:19 -0500

    clean up code

--- app/models/message.rb
+++ app/models/message.rb
@@ -27,19 +27,20 @@ class Message < ApplicationRecord
       "Function" => "GetUser",
       "Parameters" => {}
     }
-    user_response = HTTParty.post(endpoint, headers: headers, body: user_payload.to_json)
+    user_response = HTTParty.get(endpoint, headers: headers, body: user_payload.to_json)
     if user_response.code == 200
       user_id = JSON.parse(user_response.body)['UserId']
     else
       # Handle the error
       user_id = nil
     end
+
     # Create the contact
     contact_payload = {
       "Function" => "CreateContact",
       "Parameters" => {
         "IsCompany" => false,
-        "AssignedTo" => user_id,
+        "AssignedTo" => user_id,  #need to just get the USER ID so I can avoid doin the get call each time..
         "Name" => "#{self.name}",
         "Email" => "#{self.email}",
         "Phone" => "#{self.phone}",
@@ -51,21 +52,26 @@ class Message < ApplicationRecord
     
 
     response = HTTParty.post(endpoint, headers: headers, body: contact_payload.to_json)
+    contact_id = JSON.parse(response.body)['ContactId']
     
-    if response.code == 200
-      # API Contact created successfully
-      self.lacrm_contact_id = JSON.parse(response.body)['ContactId']
-    else
-      self.lacrm_contact_id = 0
-    end
-    self.lacrm_response_code = response.code
-    self.lacrm_response_body = response.body
+    # DO I REALLY NEED TO SAVE THIS INTO THE DB??
+    #if response.code == 200
+    #  # API Contact created successfully
+    #  self.lacrm_contact_id = JSON.parse(response.body)['ContactId']
+    #else
+    #  self.lacrm_contact_id = 0
+    #end
+    #self.lacrm_response_code = response.code
+    #self.lacrm_response_body = response.body
+
+    
 
     # Add a note
     note_payload = {
       "Function" => "CreateNote",
       "Parameters" => {
-        "ContactId" => "#{self.lacrm_contact_id}",
+    #    "ContactId" => "#{self.lacrm_contact_id}", #removed bc we're not saving that value into DB. 
+        "ContactId" => contact_id,
         "Note" => "#{self.body}",
       }
     }
