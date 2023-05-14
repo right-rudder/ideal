b334986fe2cd66a2aa4d97a51da2512145f3b290
Tim Jedrek <tim@timjedrek.com>
2023-05-13 23:07:16 -0500

    add recaptcha to quiz result

--- app/controllers/quiz_results_controller.rb
+++ app/controllers/quiz_results_controller.rb
@@ -22,30 +22,37 @@ class QuizResultsController < ApplicationController
   # POST /quiz_results or /quiz_results.json
   def create
     @quiz_result = QuizResult.new(quiz_result_params)
+    success = verify_recaptcha(action: 'quiz', minimum_score: 0.2, secret_key: ENV['RECAPTCHA_SECRET_KEY'])
+    checkbox_success = verify_recaptcha unless success
 
     respond_to do |format|
-      if @quiz_result.save
+      if @quiz_result.save && (success || checkbox_success)
+        # MAILER GOES HERE QuizConfirmationMailer.quiz_confirmation_email(@quiz_result).deliver_later
         format.html { redirect_to quiz_confirmation_path, notice: "#{@quiz_result.first_name}" }
-        format.json { render :show, status: :created, location: @quiz_result }
+        #format.json { render :show, status: :created, location: @quiz_result }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        format.html { 
+          if !success
+            @show_checkbox_recaptcha = true
+          end
+          render :new, status: :unprocessable_entity }
         format.json { render json: @quiz_result.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # PATCH/PUT /quiz_results/1 or /quiz_results/1.json
-  def update
-    respond_to do |format|
-      if @quiz_result.update(quiz_result_params)
-        format.html { redirect_to quiz_result_url(@quiz_result), notice: "Quiz result was successfully updated." }
-        format.json { render :show, status: :ok, location: @quiz_result }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @quiz_result.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #def update
+  #  respond_to do |format|
+  #    if @quiz_result.update(quiz_result_params)
+  #      format.html { redirect_to quiz_result_url(@quiz_result), notice: "Quiz result was successfully updated." }
+  #      format.json { render :show, status: :ok, location: @quiz_result }
+  #    else
+  #      format.html { render :edit, status: :unprocessable_entity }
+  #      format.json { render json: @quiz_result.errors, status: :unprocessable_entity }
+  #    end
+  #  end
+  #end
 
   # DELETE /quiz_results/1 or /quiz_results/1.json
   def destroy
