c467922351e4d6b5a428eb97b0f6bb43972e1f40
Tim Jedrek <tim@timjedrek.com>
2023-05-14 03:57:29 -0500

    create migration for LACRM API

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,9 @@
+require 'httparty'
+require 'json'
+
 class Message < ApplicationRecord
   before_validation :strip_phone_number
+  before_save :to_lacrm
   
   validates :name, presence: true
   validates :body, presence: { message: "Tell us how we can help" }
@@ -8,5 +12,44 @@ class Message < ApplicationRecord
 
   def strip_phone_number
     self.phone = phone.to_s.gsub(/[-() ]/, "")
+  end
+
+  def to_lacrm
+    api_key = ENV['LACRM_API']
+
+    # Set the endpoint and headers
+    endpoint = "https://api.lessannoyingcrm.com/v2/"  
+     
+    #name = self.first_name + ' ' + self.last_name <- use for forms with first name and last name fields.
+
+    headers = {
+        "Authorization" => ENV['LACRM_API'],
+        "Content-Type" => "application/json"
+    }
+
+    payload = {
+        "Function" => "CreateContact",
+        "Parameters" => {
+            "IsCompany" => false,
+            "AssignedTo" => "850228" ,  #This needs to be the USERID...
+            "Name" => "#{self.name}",
+            "Email" => "#{self.email}",
+            "Phone" => "#{self.phone}",
+            "Background Info" => "#{self.body}",
+
+        }
+    }
+
+    # Make the POST request to create the contact
+    # response = HTTParty.post(endpoint, headers: headers, body: contact_data.to_json)
+    response = HTTParty.post(endpoint, headers: headers, body: payload.to_json)
+    if response.code == 200
+      # API Contact created successfully
+      self.lacrm_contact_id = JSON.parse(response.body)['ContactId']
+    else
+      self.lacrm_contact_id = 0
+    end
+    self.lacrm_response_code = response.code
+    self.lacrm_response_body = response.body
   end
 end
