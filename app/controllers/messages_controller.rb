81e679709268e5433f1ff29fa4f7017842b37038
Tim Jedrek <tim@timjedrek.com>
2023-05-15 03:01:46 -0500

    add location to approved w/o sign in

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
   before_action :set_message, only: %i[ show edit update destroy ]
-  before_action :authenticate_admin!, except: [:create, :new, :confirmation]
+  before_action :authenticate_admin!, except: [:create, :new, :confirmation, :location]
   invisible_captcha only: [:create], honeypot: :confirm_email
 
   # GET /messages or /messages.json
