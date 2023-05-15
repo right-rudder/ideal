0c60f1f773ea94ccce6a434905dc543a79012fb5
Tim Jedrek <tim@timjedrek.com>
2023-05-15 00:27:03 -0500

    add validations to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,9 @@ class Post < ApplicationRecord
   #validates :feature_image, blob: { content_type: ['image/png', 'image/jpg', 'image/jpeg', 'image/webp'], size_range: 1..(2.megabytes) }
   
   has_one_attached :feature_image
+
+  validates :title, presence: true
+  validates :post_date, presence: true
+  validates :author, presence: true
+  validates :meta_desription, presence: true
 end
