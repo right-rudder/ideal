fa2b831c011ba29f1be9f95b3f44ccd7c7814695
Tim Jedrek <tim@timjedrek.com>
2023-05-15 02:01:45 -0500

    friendly id install"

--- Gemfile
+++ Gemfile
@@ -60,6 +60,7 @@ gem "image_processing", ">= 1.2"
 gem "aws-sdk-s3", require: false
 gem 'tinymce-rails'
 gem "devise", "~> 4.9"
+gem 'friendly_id', '~> 5.4.0'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,9 @@
 class Post < ApplicationRecord
   
+  extend FriendlyId
+  friendly_id :title, use: [:slugged, :finders]
+
+  
   #validates :feature_image, blob: { content_type: ['image/png', 'image/jpg', 'image/jpeg', 'image/webp'], size_range: 1..(2.megabytes) }
   
   has_one_attached :feature_image
@@ -8,4 +12,6 @@ class Post < ApplicationRecord
   validates :post_date, presence: true
   validates :author, presence: true
   validates :meta_desription, presence: true
+
+
 end
