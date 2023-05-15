cb5115189e9df552c2d473e2b8e05546068792a7
Tim Jedrek <tim@timjedrek.com>
2023-05-14 21:38:49 -0500

    init active storage

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  
+  validates :feature_image, blob: { content_type: ['image/png', 'image/jpg', 'image/jpeg', 'image/webp'], size_range: 1..(2.megabytes) }
+  
+  has_one_attached :feature_image
 end
