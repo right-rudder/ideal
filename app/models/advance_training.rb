54d13053ec128e75c8dcb29dccf957b37aa343a5
Robert Yamsuan Jr <rjyamsuan@gmail.com>
2023-08-17 15:58:09 +0800

    + add email validation

--- app/models/advance_training.rb
+++ app/models/advance_training.rb
@@ -5,7 +5,7 @@ class AdvanceTraining < ApplicationRecord
   validates :last_name, presence: true
   validates :comments, presence: true
   validates :phone, presence: true, format: { with: /\A\d{10}\z/, message: "must be a valid 10-digit phone number" }
-  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
 
   def strip_phone_number
       self.phone = phone.to_s.gsub(/[-() ]/, "")
