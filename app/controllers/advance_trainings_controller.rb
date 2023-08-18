7d85be6a6651796a6470d8a386ee11822855c8ef
Tim Jedrek <tim@timjedrek.com>
2023-08-17 19:59:51 -0500

    introduce private function calculate_advance_page to get the page value for advance_page which holds the value for the page its on

--- app/controllers/advance_trainings_controller.rb
+++ app/controllers/advance_trainings_controller.rb
@@ -17,6 +17,7 @@ class AdvanceTrainingsController < ApplicationController
 
   def instrument_rating
     @advance_training = AdvanceTraining.new
+    @advanced_page = calculate_advanced_page(request.path)  # Calculate advanced_page value
   end
 
 
@@ -41,32 +42,38 @@ class AdvanceTrainingsController < ApplicationController
   # POST /advance_trainings or /advance_trainings.json
   def create
     @advance_training = AdvanceTraining.new(advance_training_params)
+    #@advanced_page = session[:advanced_page]  # Retrieve advanced_page value from the session
   
     respond_to do |format|
       if @advance_training.save
+        # Handle successful submission
         format.html { redirect_to advanced_training_confirmation_path, notice: @advance_training[:first_name], alert: @advance_training[:certificate_sought] }
         format.json { render :show, status: :created, location: @advance_training }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        format.html { render action: controller.controller_name } # Render the same action as the form
         format.json { render json: @advance_training.errors, status: :unprocessable_entity }
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace("advance_training_form", partial: "form", locals: { advance_training: @advance_training, advanced_page: @advanced_page })
+        end
       end
     end
   end
+  
   
   
 
   # PATCH/PUT /advance_trainings/1 or /advance_trainings/1.json
-  def update
-    respond_to do |format|
-      if @advance_training.update(advance_training_params)
-        format.html { redirect_to advance_training_url(@advance_training), notice: "Advance training was successfully updated." }
-        format.json { render :show, status: :ok, location: @advance_training }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @advance_training.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #def update
+  #  respond_to do |format|
+  #    if @advance_training.update(advance_training_params)
+  #      format.html { redirect_to advance_training_url(@advance_training), notice: "Advance training was successfully updated." }
+  #      format.json { render :show, status: :ok, location: @advance_training }
+  #    else
+  #      format.html { render :edit, status: :unprocessable_entity }
+  #      format.json { render json: @advance_training.errors, status: :unprocessable_entity }
+  #    end
+  #  end
+  #end
 
   # DELETE /advance_trainings/1 or /advance_trainings/1.json
   def destroy
@@ -79,6 +86,22 @@ class AdvanceTrainingsController < ApplicationController
   end
 
   private
+
+    def calculate_advanced_page(path)
+      case path
+      when instrument_rating_path
+        "Instrument Rating"
+      when commercial_certificate_path
+        "Commercial Certificate"
+      when multi_engine_rating_path
+        "Multi Engine Rating"
+      when certified_flight_instructor_path
+        "CFI Certificate"
+      else
+        "Default Value"  # Set an appropriate default value
+      end
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_advance_training
       @advance_training = AdvanceTraining.find(params[:id])
