d15967420c92cc2d0c280c4890e9c584028e2f6f
Tim Jedrek <tim@timjedrek.com>
2023-08-21 23:39:41 -0500

    activate previewer

--- test/mailers/previews/advance_trainings_mailer_preview.rb
+++ test/mailers/previews/advance_trainings_mailer_preview.rb
@@ -3,7 +3,8 @@ class AdvanceTrainingsMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/advance_trainings_mailer/advance_trainings
   def advance_trainings
-    AdvanceTrainingsMailer.advance_trainings
+    advance_training = AdvanceTraining.last
+    AdvanceTrainingsMailer.advance_trainings(advance_training)
   end
 
 end
