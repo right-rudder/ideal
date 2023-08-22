320325628ce532a773d102c4a52269f2a3a0ca9e
Tim Jedrek <tim@timjedrek.com>
2023-08-21 23:41:00 -0500

    change subject again

--- app/mailers/advance_trainings_mailer.rb
+++ app/mailers/advance_trainings_mailer.rb
@@ -6,8 +6,8 @@ class AdvanceTrainingsMailer < ApplicationMailer
     @advance_training = advance_training
 
     # Comment out to not include info@idealaviationstl.com for testing.
-    # mail(to: @advance_training.email, bcc: ["info@idealaviationstlouis.com", "info@idealaviationstl.com"], subject: "✈️ Requesting Information About Ideal Aviation's #{@advance_training.certificate_sought}")
-    mail(to: @advance_training.email, bcc: ["info@idealaviationstlouis.com"], subject: "✈️ Requesting Information About Ideal Aviation's #{@advance_training.certificate_sought}")
+    # mail(to: @advance_training.email, bcc: ["info@idealaviationstlouis.com", "info@idealaviationstl.com"], subject: "✈️ Complete your #{@advance_training.certificate_sought} at Ideal Aviation")
+    mail(to: @advance_training.email, bcc: ["info@idealaviationstlouis.com"], subject: "✈️ Complete your #{@advance_training.certificate_sought} at Ideal Aviation")
   end
 
 end
