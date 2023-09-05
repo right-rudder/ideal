e6714f42df56acf1dcc450a54370da3f7d2d4c0c
Tim Jedrek <tim@timjedrek.com>
2023-09-05 16:05:04 -0500

    update mailer in prepration to go live

--- app/mailers/advance_trainings_mailer.rb
+++ app/mailers/advance_trainings_mailer.rb
@@ -5,9 +5,7 @@ class AdvanceTrainingsMailer < ApplicationMailer
   def advance_trainings(advance_training)
     @advance_training = advance_training
 
-    # Comment out to not include info@idealaviationstl.com for testing.
-    # mail(to: @advance_training.email, bcc: ["info@idealaviationstlouis.com", "info@idealaviationstl.com"], subject: "✈️ Complete your #{@advance_training.certificate_sought} at Ideal Aviation")
-    mail(to: @advance_training.email, bcc: ["info@idealaviationstlouis.com"], subject: "✈️ Complete your #{@advance_training.certificate_sought} at Ideal Aviation")
+    mail(to: @advance_training.email, bcc: ["info@idealaviationstlouis.com", "info@idealaviationstl.com"], subject: "✈️ Complete your #{@advance_training.certificate_sought} at Ideal Aviation")
   end
 
 end
