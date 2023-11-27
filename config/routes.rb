7e70952efdc29442555576832d49087f2a924697
Tim Jedrek <tim@timjedrek.com>
2023-11-27 12:48:37 -0600

    remove rentals

--- config/routes.rb
+++ config/routes.rb
@@ -60,7 +60,7 @@ Rails.application.routes.draw do
   # Static Pages
   get 'how-to-become-a-pilot', to: 'pages#become_a_pilot', as: :become
   get 'pilot-programs', to: 'pages#pilot_programs', as: :programs
-  get 'aircraft-rental', to: 'pages#aircraft_rental', as: :aircraft_rental
+  #get 'aircraft-rental', to: 'pages#aircraft_rental', as: :aircraft_rental
   get 'fixed-wing-airplane-fleet', to: 'pages#fixed_wing', as: :fixed_wing
   get 'rotary-wing-helicopter-fleet', to: 'pages#rotary_wing', as: :rotary_wing
   get 'airplane-pilot-training', to: 'pages#airplane_training', as: :airplane_training
